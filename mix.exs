b28bf77f2b55f0034993e06ae19985597af21f92
Deans Charbal <deansfa@gmail.com>
2015-08-04 00:28:07 -0400

    Make a command-line executable.

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Issues.Mixfile do
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     escript: escript_config,
      deps: deps]
   end
 
@@ -33,5 +34,9 @@ defmodule Issues.Mixfile do
       {:exvcr, "~> 0.4.1", only: :test},
       {:ibrowse, github: "cmullaparthi/ibrowse", only: :test}
     ]
+  end
+
+  defp escript_config do
+    [ main_module: Issues.CLI ]
   end
 end
